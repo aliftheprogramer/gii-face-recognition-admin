43fdf29e66a5f32adaa8a842b35ce78c185dca45
aliftheprogramer <alifarya679@gmail.com>
2025-11-19 01:47:01 +0700

    ewjgwiuygf

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,8 @@
-<!-- app/layouts/default.vue -->
-
 <template>
   <div class="flex min-h-screen bg-[#EFEFEF] font-sans text-gray-900">
     <SideNavbar />
 
-    <main class="flex-1 ml-64 p-8">
+    <main class="flex-1 ml-64 py-8 px-[36px]">
       <slot />
     </main>
   </div>
